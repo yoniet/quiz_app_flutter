65929f3efdcb715d2ecbbf4fafdccf796d91da29
yoniet <yonie812@gmail.com>
2023-12-17 15:27:25 +0200

    Adding another classes and styles for applicatoin quiz

---
+++ lib/answer_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(
+      {super.key, required this.answerText, required this.onClick});
+
+  final String answerText;
+  final void Function() onClick;
+
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+        onPressed: onClick,
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+          backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+          foregroundColor: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+        ),
+        child: Text(
+          answerText,
+          textAlign: TextAlign.center,
+        ));
+  }
+}
